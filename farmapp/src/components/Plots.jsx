d555fc7ef9439c6b1e6e6c265a57c8ec67e56bdb
rbbristow <rebecca.bristow@outlook.fr>
2021-11-10 12:29:21 +0100

    update plot grid css

--- farmapp/src/components/Plots.jsx
+++ farmapp/src/components/Plots.jsx
@@ -6,9 +6,17 @@ class Plot extends React.Component {
             <div className="plot-div">
                 <div className="plot-grid">
                     <div class="plot-grid-item">
+                        <button className="button-row"><span className=".button-row-text">+</span></button>
+                        <button className="button-row"><span className=".button-row-text">pumpkin</span></button>
+                        <button className="button-row"><p className=".button-row-text">pumpkin</p></button>
+                        <button className="button-row"><p className=".button-row-text">pumpkin</p></button>
+                        <button className="button-row"><p className=".button-row-text">pumpkin</p></button>
+                        <button className="button-row"><p className=".button-row-text">pumpkin</p></button>
                         <button className="button-row"><p className=".button-row-text">pumpkin</p></button>
                     </div>
-                    <div class="plot-grid-item">2</div>
+                    <div class="plot-grid-item">
+                    <button className="button-row"><p className=".button-row-text">+</p></button>
+                    </div>
                     <div class="plot-grid-item">3</div>
                     <div class="plot-grid-item">4</div>
                     <div class="plot-grid-item">5</div>
