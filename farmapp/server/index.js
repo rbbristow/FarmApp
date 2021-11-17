a874366e92db05b86a5981954e6f5bd8feaa96ac
rbbristow <rebecca.bristow@outlook.fr>
2021-11-17 11:57:16 +0100

    create mysql database

--- farmapp/server/index.js
+++ farmapp/server/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'admin',
+    password: 'password',
+    database: 'FarmApp'
+});
 
 app.get('/', (req, res) => {
     res.send("Hello Rebex")
-})
+});
 
 app.listen(3001, () => {
     console.log('running on port 3001')
-})
+});
